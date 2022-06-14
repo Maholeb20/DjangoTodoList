7be0027bfd190381dd06ac530594d877eb021c2c
DA M1 36 <dam136@MAC-M1-32.local>
2022-06-14 16:09:23 +0200

    first crud App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DeleteComponent } from './component/crud/delete/delete.component';
+import { UpdateComponent } from './component/crud/update/update.component';
+import { AddComponent } from './component/crud/add/add.component';
+import { HomeComponent } from './component/home/home.component';
+
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeleteComponent,
+    UpdateComponent,
+    AddComponent,
+    HomeComponent,
+
+    NavbarComponent,
+     FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
